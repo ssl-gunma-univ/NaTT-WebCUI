14d31c596b911f7a8fba9ebcfa7f16b205552d29
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2022-12-25 10:29:59 +0900

    clean

--- api/webcui_lib.php
+++ api/webcui_lib.php
@@ -1,13 +1,11 @@
 <?php
 
-//'' で囲った文字列
 function getString($paramName) {
     $param = $_POST[$paramName];
     $param = escapeshellarg($param);
     return $param;
 }
 
-//'' で囲ったファイル名のみ
 function getFilename($paramName) {
     $param = $_POST[$paramName];
     $param = basename($param);
@@ -29,12 +27,10 @@ function getFloat($paramName) {
 //     echo str_replace(" ", "&nbsp;", htmlspecialchars($line, ENT_QUOTES)) . "<br>";
 // }
 
-//文字列をhtml形式に変換
 function printLine($line) {
     echo str_replace("font&nbsp;", "font ", str_replace(" ", "&nbsp;", $line)) . "<br>";
 }
 
-//html形式で表示
 function printOutput($output) {
     for ($i = 0; $i < count($output); $i++) {
         printLine($output[$i]);
